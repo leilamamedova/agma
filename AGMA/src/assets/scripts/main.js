8b361496a7876d1c5b07fa6505c85e9b44cf9818
leilamamedova <leila.m26ova@gmail.com>
2022-05-15 00:53:55 +0400

    Revert "front side"
    This reverts commit 1262d16a07feffe2523c0403ceb2f942cd0091ec.

--- AGMA/src/assets/scripts/main.js
+++ AGMA/src/assets/scripts/main.js
@@ -1,28 +1,3 @@
-$(document).ready(function () {
-  $(".video-gallery").magnificPopup({
-    delegate: "a",
-    type: "iframe",
-    gallery: {
-      enabled: true
-    }
-  });
-});
-
-/* Trainings PopUp - Input Copy */
-function clipboard(elem, event) {
-  elem.prev('input[type="text"]').focus().select();
-  document.execCommand(event);
-  elem.prev('input[type="text"]').blur();
-  elem.addClass('clicked');
-  setTimeout(function(){
-    elem.removeClass('clicked');
-  }, 500);
-}
-
-$('.btn-copy').on('click', function(){
-  clipboard($(this), 'copy')
-});
-
 // Carousel
 $('#header__info__carousel').owlCarousel({
   margin: 20,
@@ -533,4 +508,3 @@ function loadFile (event) {
   var image = document.getElementById("output");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
-
